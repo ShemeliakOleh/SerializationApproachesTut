4113dd0c5dc37204ed233d902605e8dc54239c83
Oleh Shemeliak <Shemeliak.Oleh.Main@gmail.com>
2022-06-17 20:53:38 +0300

    Add project files.

---
+++ SerializationApproaches/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using SerializationApproaches;
+
+Person person = new() { FirstName = "Ivan", Age = 27 };
+
+
+
+Serializer serializer = new Serializer();
+
+//Binary Serialization
+//string binaryFilePath = "binarydata.txt";
+//serializer.SerializeToFileAsBinary(person, binaryFilePath);
+//var personFromBinaryFile = serializer.DeserializeFromFileAsBinary(binaryFilePath) as Person;
+//Console.WriteLine(personFromBinaryFile);
+
+//Xml Serialization
+//string xmlFilePath = "xmldata.txt";
+//serializer.SerializeToFileAsXml(typeof(Person), person, xmlFilePath);
+//var personFromXmlFile = serializer.DeserializeFromFileAsXml(typeof(Person),xmlFilePath);
+//Console.WriteLine(personFromXmlFile);
+
+//Json Serialization
+//string jsonFilePath = "jsondata.txt";
+//serializer.SerializeToFileAsJson(typeof(Person), person, jsonFilePath);
+//var personFromJsonFile = serializer.DeserializeFromFileAsJson(typeof(Person), jsonFilePath);
+//Console.WriteLine(personFromJsonFile);
+
---
+++ SerializationApproaches/Serializer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializationApproaches
+{
+    public class Serializer
+    {
+        public void SerializeToFileAsBinary(object data, string filePath)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            PrepareFile(filePath);
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                binaryFormatter.Serialize(fs, data);
+            }
+
+        }
+
+        public object? DeserializeFromFileAsBinary(string filePath)
+        {
+            object? obj = null;
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    obj = binaryFormatter.Deserialize(fs);
+                }
+            }
+            return obj;
+        }
+
+
+        public void SerializeToFileAsXml(Type type, object data, string filePath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(type);
+
+            PrepareFile(filePath);
+            using (TextWriter writer = new StreamWriter(filePath))
+            {
+                xmlSerializer.Serialize(writer, data);
+            }
+        }
+
+        public object? DeserializeFromFileAsXml(Type type, string filePath)
+        {
+            object? obj = null;
+            XmlSerializer xmlSerializer = new XmlSerializer(type);
+
+            if (File.Exists(filePath))
+            {
+                using (TextReader reader = new StreamReader(filePath))
+                {
+                    obj = xmlSerializer.Deserialize(reader);
+                }
+            }
+
+            return obj;
+        }
+
+
+
+
+
+        public void SerializeToFileAsJson(Type type, object data, string filePath)
+        {
+            PrepareFile(filePath);
+            using (FileStream writer = new FileStream(filePath, FileMode.Create))
+            {
+                JsonSerializer.Serialize(writer, data, type);
+            }
+
+        }
+
+        public object? DeserializeFromFileAsJson(Type type,string filePath)
+        {
+            object? obj = null;
+
+            if (File.Exists(filePath))
+            {
+                using (FileStream reader = new FileStream(filePath, FileMode.Open))
+                {
+                    obj = JsonSerializer.Deserialize(reader, type);
+                }
+            }
+            return obj;
+        }
+
+
+
+
+        private void PrepareFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+        }
+    }
+}
